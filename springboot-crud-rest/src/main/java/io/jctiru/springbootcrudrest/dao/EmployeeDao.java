40b801d7cb787d173194a226118e96bd6382f17c
jctiru <jonathanchristiantiru@gmail.com>
2019-09-24 17:47:37 +0800

    Add crud methods on dao

--- springboot-crud-rest/src/main/java/io/jctiru/springbootcrudrest/dao/EmployeeDao.java
+++ springboot-crud-rest/src/main/java/io/jctiru/springbootcrudrest/dao/EmployeeDao.java
@@ -5,7 +5,13 @@ import java.util.List;
 import io.jctiru.springbootcrudrest.entity.Employee;
 
 public interface EmployeeDao {
-	
+
 	public List<Employee> findAll();
+
+	public Employee findById(int id);
+
+	public void save(Employee employee);
+
+	public void deleteById(int id);
 
 }
--- springboot-crud-rest/src/main/java/io/jctiru/springbootcrudrest/dao/EmployeeDaoHibernateImpl.java
+++ springboot-crud-rest/src/main/java/io/jctiru/springbootcrudrest/dao/EmployeeDaoHibernateImpl.java
@@ -19,13 +19,33 @@ public class EmployeeDaoHibernateImpl implements EmployeeDao {
 	private EntityManager entityManager;
 
 	@Override
-	@Transactional
 	public List<Employee> findAll() {
 		Session currentSession = entityManager.unwrap(Session.class);
 		Query<Employee> query = currentSession.createQuery("from Employee", Employee.class);
 		List<Employee> employeeList = query.getResultList();
 
 		return employeeList;
+	}
+
+	@Override
+	public Employee findById(int id) {
+		Session currentSession = entityManager.unwrap(Session.class);
+		Employee employee = currentSession.get(Employee.class, id);
+		return employee;
+	}
+
+	@Override
+	public void save(Employee employee) {
+		Session currentSession = entityManager.unwrap(Session.class);
+		currentSession.saveOrUpdate(employee);
+	}
+
+	@Override
+	public void deleteById(int id) {
+		Session currentSession = entityManager.unwrap(Session.class);
+		Query query = currentSession.createQuery("delete from Employee where id=:id");
+		query.setParameter("id", id);
+		query.executeUpdate();
 	}
 
 }

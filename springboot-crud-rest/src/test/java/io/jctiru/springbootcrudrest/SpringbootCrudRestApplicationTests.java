001901e888c9312d96b8aadce822b97dc6362c20
jctiru <jonathanchristiantiru@gmail.com>
2019-10-02 16:49:36 +0800

    Add test case for find by first name

--- springboot-crud-rest/src/test/java/io/jctiru/springbootcrudrest/SpringbootCrudRestApplicationTests.java
+++ springboot-crud-rest/src/test/java/io/jctiru/springbootcrudrest/SpringbootCrudRestApplicationTests.java
@@ -1,16 +1,31 @@
 package io.jctiru.springbootcrudrest;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import io.jctiru.springbootcrudrest.dao.EmployeeRepository;
+import io.jctiru.springbootcrudrest.entity.Employee;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringbootCrudRestApplicationTests {
 
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void testFindByFirstName() {
+		List<Employee> employeeList = employeeRepository.findByLastName("Salvador");
+		employeeList.forEach(e -> System.out.println(e));
 	}
 
 }

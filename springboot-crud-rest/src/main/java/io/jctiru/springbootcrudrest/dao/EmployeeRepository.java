7f7a7651d46d449cb4a8026c68d527413a163564
jctiru <jonathanchristiantiru@gmail.com>
2019-10-03 00:29:58 +0800

    Add find all by repository methods

--- springboot-crud-rest/src/main/java/io/jctiru/springbootcrudrest/dao/EmployeeRepository.java
+++ springboot-crud-rest/src/main/java/io/jctiru/springbootcrudrest/dao/EmployeeRepository.java
@@ -8,6 +8,17 @@ import io.jctiru.springbootcrudrest.entity.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
+	/*
+	 * Spring Data’s method parsing uses prefix keywords like find, exists, count,
+	 * and delete and a terminating By keyword. Everything you put in between find
+	 * and By makes your method name more expressive and does not affect query
+	 * derivation.
+	 */
+
 	List<Employee> findByLastName(String lastName);
+
+	List<Employee> findAllByOrderByLastName();
+
+	List<Employee> findAllByOrderByLastNameDesc();
 
 }

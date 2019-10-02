a3772a05d78415017853f04d47bb12e87977ee03
jctiru <jonathanchristiantiru@gmail.com>
2019-10-02 21:06:46 +0800

    Add test case for find all paging

--- springboot-crud-rest/src/test/java/io/jctiru/springbootcrudrest/SpringbootCrudRestApplicationTests.java
+++ springboot-crud-rest/src/test/java/io/jctiru/springbootcrudrest/SpringbootCrudRestApplicationTests.java
@@ -6,6 +6,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import io.jctiru.springbootcrudrest.dao.EmployeeRepository;
@@ -26,6 +29,13 @@ public class SpringbootCrudRestApplicationTests {
 	public void testFindByFirstName() {
 		List<Employee> employeeList = employeeRepository.findByLastName("Salvador");
 		employeeList.forEach(e -> System.out.println(e));
+	}
+
+	@Test
+	public void testFindAllPaging() {
+		Pageable pageable = PageRequest.of(9, 1);
+		Page<Employee> results = employeeRepository.findAll(pageable);
+		results.forEach(e -> System.out.println(e));
 	}
 
 }

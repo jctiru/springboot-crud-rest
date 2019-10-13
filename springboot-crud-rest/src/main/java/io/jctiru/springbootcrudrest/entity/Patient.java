bdd2d582bdeadb37db88a57e81d237786b53e638
jctiru <jonathanchristiantiru@gmail.com>
2019-10-13 23:26:34 +0800

    Create patient entity

---
+++ springboot-crud-rest/src/main/java/io/jctiru/springbootcrudrest/entity/Patient.java
@@ -0,0 +1,61 @@
+package io.jctiru.springbootcrudrest.entity;
+
+public class Patient {
+
+	private long id;
+
+	private String firstName;
+
+	private String lastName;
+
+	private String phone;
+
+	private Insurance insurance;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public Insurance getInsurance() {
+		return insurance;
+	}
+
+	public void setInsurance(Insurance insurance) {
+		this.insurance = insurance;
+	}
+
+	@Override
+	public String toString() {
+		return "Patient [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", phone=" + phone
+				+ ", insurance=" + insurance + "]";
+	}
+
+}

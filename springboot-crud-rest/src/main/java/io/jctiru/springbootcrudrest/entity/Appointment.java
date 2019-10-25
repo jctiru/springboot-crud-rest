a429268ad694b64d34654860171af7cc8285cdbf
jctiru <jonathanchristiantiru@gmail.com>
2019-10-25 23:11:56 +1000

    Add missing entity and table annotation in appointment entity

--- springboot-crud-rest/src/main/java/io/jctiru/springbootcrudrest/entity/Appointment.java
+++ springboot-crud-rest/src/main/java/io/jctiru/springbootcrudrest/entity/Appointment.java
@@ -4,12 +4,16 @@ import java.sql.Timestamp;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "appointment")
 public class Appointment {
 
 	@Id
